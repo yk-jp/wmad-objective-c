ad36e9ca64421f20ea690aa59da025665be7d073
yk-jp <k82.y12@gmail.com>
2022-08-30 20:47:23 -0700

    initial commit

---
+++ TestObjc/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestObjc
+//
+//  Created by Yusuke K on 2022-08-29.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
